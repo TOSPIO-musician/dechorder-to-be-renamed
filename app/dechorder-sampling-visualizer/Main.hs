1a036b3f40d163bd1e49250744012277a623a0cf
Yukio Usuzumi <anohigisavay@gmail.com>
2018-02-25 05:25:39 +0800

    Coding style

--- app/dechorder-sampling-visualizer/Main.hs
+++ app/dechorder-sampling-visualizer/Main.hs
@@ -11,9 +11,9 @@ signal xs = [ (x,(sin (x*3.14159/45) + 1) / 2 * (sin (x*3.14159/5))) | x <- xs ]
 
 main :: IO ()
 main = do
-  let renderable = (toRenderable (execEC $ do
-                                     layout_title .= "Amplitude Modulation"
-                                     setColors [opaque blue, opaque red]
-                                     plot (line "am" [signal [0,(0.5)..400]])
-                                     plot (points "am points" (signal [0,7..400]))))
+  let renderable = toRenderable $ execEC $ do
+        layout_title .= "Amplitude Modulation"
+        setColors [opaque blue, opaque red]
+        plot (line "am" [signal [0,(0.5)..400]])
+        plot (points "am points" (signal [0,7..400]))
   renderableToWindow renderable 800 600
